cf4a49919481c1eab3ca9fcc460780a37b2bbf4c
alex <alex.sd3@gmail.com>
2020-01-10 15:04:40 +0100

    add Docker.webpack

--- settings.example.py
+++ settings.example.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 # logging
 LOGS_ROOT = env('LOGS_ROOT', default=root_path('logs'))
